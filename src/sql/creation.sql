84a39d48affa7cfe6d5dc4c60c3a066348da4786
alepalenc <alepalencia98@gmail.com>
2019-12-11 16:05:22 +0100

    actualizados coches_query.html creation.sql, creado error.html

--- src/sql/creation.sql
+++ src/sql/creation.sql
@@ -1,53 +1,48 @@
-CREATE TABLE if not exists cliente (
-    dni VARCHAR(9) PRIMARY KEY, 
+CREATE TABLE cliente(
+    dni VARCHAR(9) PRIMARY KEY,
     nombre VARCHAR(20), 
     telefono VARCHAR(20)
 );
 
-create table if not exists coche(
-    id_coche integer primary key,
-    numero_bastidor varchar(17) unique,
-    matricula varchar(7) unique,
-    marca varchar(20) not null,
-    modelo varchar(20) not null,
-    color varchar(12) not null,
-    estado varchar(256)
+CREATE TABLE IF NOT EXISTS coche(
+    id_coche SERIAL PRIMARY KEY,
+    numero_bastidor VARCHAR(17) UNIQUE,
+    matricula VARCHAR(7) UNIQUE,
+    marca VARCHAR(20) NOT NULL,
+    modelo VARCHAR(20) NOT NULL,
+    color VARCHAR(12) NOT NULL,
+    estado VARCHAR(256)
 );
 
-CREATE TABLE if not exists alquiler (
-    dni varchar(9), 
-    id_coche integer ,
+CREATE TABLE IF NOT EXISTS alquiler(
+    dni VARCHAR(9), 
+    id_coche INTEGER,
     fecha_inicio DATE, 
     fecha_fin DATE,
     precio FLOAT,
-    estado VARCHAR(10) check (estado in ('pagado', 'no_pagado')),
+    estado VARCHAR(10) CHECK (estado IN ('pagado', 'no_pagado')),
+
     PRIMARY KEY (dni, id_coche, fecha_inicio),
-
-    foreign key (dni) references cliente(dni) on delete cascade,
-    foreign key (id_coche) references coche(id_coche) on delete cascade
-    
+    FOREIGN KEY (dni) REFERENCES cliente(dni) ON DELETE CASCADE,
+    FOREIGN KEY (id_coche) REFERENCES coche(id_coche) ON DELETE CASCADE
 );
 
-create table if not exists proveedores(
-       cif varchar(9) not null primary key,
-       nombre varchar(64) not null,
-       ubicacion varchar(64) not null,
-       telefono integer not null,
-       correo varchar(64) not null,
-       created date not null default CURRENT_TIMESTAMP
-
+CREATE TABLE IF NOT EXISTS proveedor(
+       cif VARCHAR(9) PRIMARY KEY,
+       nombre VARCHAR(64) NOT NULL,
+       ubicacion VARCHAR(64) NOT NULL,
+       telefono INTEGER NOT NULL,
+       correo VARCHAR(64) NOT NULL,
+       created DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
-create table if not exists solicitud(
-
-       cif varchar(9),
-       id_coche integer PRIMARY KEY,
-       fecha_entrega date,
-       punto_recogida varchar(64) not null,
+CREATE TABLE IF NOT EXISTS solicitud(
+       cif VARCHAR(9),
+       id_coche INTEGER PRIMARY KEY,
+       fecha_entrega DATE,
+       punto_recogida VARCHAR(64) NOT NULL,
        
-       foreign key (id_coche) references coche(id_coche) on delete cascade,
-       foreign key (cif) references proveedores(cif) on delete cascade
-       
+       FOREIGN KEY (id_coche) REFERENCES coche(id_coche) ON DELETE CASCADE,
+       FOREIGN KEY (cif) REFERENCES proveedor(cif) ON DELETE CASCADE
 );
-
