ecdfb0b432749695143905bf36a62ad4d485391e
Yabir G <yabirgb@gmail.com>
2019-12-13 03:38:21 +0100

    Edita y elmina proveedores

--- src/proveedores/methods.py
+++ src/proveedores/methods.py
@@ -13,25 +13,25 @@ conn = pg.connect(
 proveedores = Blueprint('proveedores', __name__,
                         template_folder='../templates', url_prefix="/proveedores")
 
-@proveedores.route('/consulta', methods=['GET','POST', 'DELETE'])
+@proveedores.route('/consulta', methods=['GET','POST'])
 def consultar_proveedores():
 
     #print(request.form)
     nombre = request.form.get('nombre', default='', type=str)
     CIF = request.form.get('cif', default='', type=str)
 
-    if request.method == 'DELETE':
-        CIF = request.form.get('cif', type=str)
+    if request.method == 'POST' and 'eliminar' in request.form.get('submit_button', ''):
+        CIF = request.form['submit_button'].split('-')[1]
         cur = conn.cursor()
-        cur.execute("DELETE FROM proveedor where CIF=%s cascade;", (CIF,))
+        cur.execute("DELETE FROM proveedor where CIF=%s;", (CIF,))
         cur.close()
 
         return render_template('proveedores_query.html', data=[], deleted=f"Proveedor con {CIF} borrado correctamente")
 
     print("data: ", nombre, CIF)
 
-    if nombre == '' and CIF == '' or request.method=='GET':
-        return render_template('proveedores_query.html', data=[])
+    if (nombre == '' and CIF == '') or request.method=='GET':
+        return render_template('proveedores_query.html', get=True)
 
     cur = conn.cursor()
     try:
@@ -44,6 +44,7 @@ def consultar_proveedores():
         return render_template('proveedores_query.html', data=data)
     except:
         cur.close()
+        conn.rollback()
         return render_template('proveedores_query.html', errors=[])
 
 @proveedores.route("/crear", methods=["GET", "POST"])
@@ -74,7 +75,6 @@ def crear_proveedor():
     cur.execute(sql, (CIF, nombre, ubicacion,telefono,correo))
     conn.commit()
     cur.close()
-    conn.close()
 
     return render_template("proveedores_crear.html", success=True)
     
@@ -142,6 +142,80 @@ def solicitar():
 
     
  
-@proveedores.route("/solicitudes", methods=["POST", "GET"])
-def busqueda_solicitud():
-    pass
+@proveedores.route("/editar/<cif>", methods=["POST", "GET"])
+def editar_proveedor(cif):
+
+    try:
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM proveedor where cif=%s", (cif, ))
+        
+        data = cur.fetchone()
+        cur.close()
+    except:
+        cur.close()
+        conn.rollback()
+        return render_template("proveedores_editar.html", error="Error en el sistema")
+        
+    if data == None:
+        return render_template("proveedores_editar.html", error="CIF no encontrado")
+
+    q = dict()
+    print(data)
+    q['cif'] = data[0]
+    q['nombre'] = data[1]
+    q['ubicacion'] = data[2]
+    q['telefono'] = data[3]
+    q['correo'] = data[4]
+    
+    if request.method == 'GET':
+        return render_template("proveedores_editar.html", data=q)    
+
+    nombre = request.form.get("nombre", type=str)
+    ubicacion = request.form.get("ubicacion", type=str)
+    telefono = request.form.get("telefono", type=int)
+    correo = request.form.get("correo", type=str)
+    CIF = request.form.get("cif", type=str)
+
+
+    # check that all the fields are fulfilled
+    if not all([nombre, ubicacion, telefono, correo, CIF]):
+        m = "Alguno de los campos no ha sido introducido correctamente"
+        back = "/proveedores/editar/"+cif
+        return render_template("error.html", message=m, back=back)
+
+    # insert in the db
+
+
+    sql = "UPDATE proveedor set cif=%s, nombre=%s, ubicacion=%s, telefono=%s, correo=%s where cif=%s"
+
+    
+    cur = conn.cursor()
+    cur.execute(sql, (CIF, nombre, ubicacion,telefono,correo, cif))
+    conn.commit()
+    cur.close()
+
+    try:
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM proveedor where cif=%s", (cif, ))
+        
+        data = cur.fetchone()
+        cur.close()
+    except:
+        cur.close()
+        conn.rollback()
+        return render_template("proveedores_editar.html", error="Error en el sistema")
+        
+    if data == None:
+        return render_template("proveedores_editar.html", error="CIF no encontrado")
+
+    q = dict()
+    print(data)
+    q['cif'] = data[0]
+    q['nombre'] = data[1]
+    q['ubicacion'] = data[2]
+    q['telefono'] = data[3]
+    q['correo'] = data[4]
+
+    return render_template("proveedores_editar.html", success="Actualizacion correcta", data = q)   
