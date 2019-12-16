9852d29862b57529e44b5d31801d5cff0c6d217b
Norberto Fernandez de la Higuera <norbertoalfa@correo.ugr.es>
2019-12-16 15:32:46 +0100

    Tuplas no validas corregidas

--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -8,15 +8,15 @@ INSERT INTO coche(marca, modelo, color) VALUES ('marca2', 'modelo1', 'color1') O
 INSERT INTO coche(marca, modelo, color) VALUES ('marca1', 'modelo2', 'color1') ON CONFLICT DO NOTHING;
 
 
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '1', '1/1/2000', '1/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '1', '1/1/2000', '1/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678C', '2', '1/1/2000', '1/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678D', '2', '1/1/2000', '1/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '1', '1/1/2000', '5/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '1', '1/2/2000', '6/2/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678C', '2', '1/1/2000', '5/1/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678D', '2', '1/2/2000', '6/2/2000', '2', 'pagado') ON CONFLICT DO NOTHING;
 
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '2', '1/2/2000', '1/2/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '3', '1/2/2000', '1/2/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '1', '1/2/2000', '1/2/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '2', '1/2/2000', '1/2/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
-INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678C', '1', '1/2/2000', '1/2/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '2', '1/3/2000', '7/3/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678A', '3', '1/1/2000', '5/1/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '1', '1/3/2000', '7/3/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678B', '2', '1/4/2000', '8/4/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
+INSERT INTO alquiler(dni,id_coche,fecha_inicio, fecha_fin, precio, estado) VALUES ('12345678C', '1', '1/4/2000', '8/4/2000', '2', 'no_pagado') ON CONFLICT DO NOTHING;
 
 INSERT INTO proveedor(cif, nombre, ubicacion, telefono, correo) VALUES ('12345678A', 'autoslocos', 'pulianas', 660816891, 'example@example.com') ON CONFLICT DO NOTHING;
